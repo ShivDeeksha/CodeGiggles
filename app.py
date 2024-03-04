82e4728580bc7d2f6b28702a3fefb4b03f836022
Disha <shivdeeksha1@gmail.com>
2024-03-04 13:18:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, send_file
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/blogs')
+def blogs():
+    return render_template('blogs.html')
+
+@app.route('/singleblog')
+def singleblog():
+    return render_template('singleblog.html')
+
+@app.route('/resume')
+def resume():
+    return send_file('static/resume.pdf')
+
+if __name__ == '__main__':
+    app.run(debug=True)
